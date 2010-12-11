36cf8ea49c1ccafb6711d58daa0d5ff3d3794550
Devin Canterberry <channel-alpha-546873456@aeon.twuni.org>
2010-12-11 10:01:36 -0800

    Refactoring generic OAuth logic around client creation into the oauth4java project, thus simplifying the implementation of ObsidianClient creation.

--- src/main/java/org/twuni/obsidian/client/ObsidianClientFactory.java
+++ src/main/java/org/twuni/obsidian/client/ObsidianClientFactory.java
@@ -1,58 +1,26 @@
 package org.twuni.obsidian.client;
 
-import java.net.URL;
-
-import org.twuni.authentication.oauth.AccessToken;
 import org.twuni.authentication.oauth.ConsumerToken;
-import org.twuni.authentication.oauth.RequestToken;
-import org.twuni.obsidian.repository.TokenRepository;
+import org.twuni.authentication.oauth.client.OAuthClientFactory;
+import org.twuni.authentication.oauth.repository.TokenRepository;
 
-public class ObsidianClientFactory {
-
-	private final ConsumerToken consumerToken;
-	private final TokenRepository tokenRepository;
+public class ObsidianClientFactory extends OAuthClientFactory<ObsidianClient> {
 
 	public ObsidianClientFactory( String consumerKey, String consumerSecret, TokenRepository tokenRepository ) {
-		this.consumerToken = new ConsumerToken( consumerKey, consumerSecret );
-		this.tokenRepository = tokenRepository;
+		this( new ConsumerToken( consumerKey, consumerSecret ), tokenRepository );
 	}
 
-	public ObsidianClient createInstance( String key, URL callbackUrl ) {
-
-		ObsidianClient client = new ObsidianClient( consumerToken, callbackUrl.toString() );
-
-		AccessToken accessToken = tokenRepository.getAccessToken( key );
-
-		if( accessToken == null ) {
-
-			RequestToken requestToken = client.getRequestToken();
-			tokenRepository.setRequestToken( key, requestToken );
-
-			String authorizationUrl = client.getAuthorizationUrl( requestToken );
-			throw new AuthorizationRequiredException( authorizationUrl );
-
-		}
-
-		client.setAccessToken( accessToken );
-
-		return client;
-
+	public ObsidianClientFactory( ConsumerToken consumerToken, TokenRepository tokenRepository ) {
+		super( consumerToken, tokenRepository );
 	}
 
-	public ObsidianClient createInstance( String key, String verifier ) {
-
-		ObsidianClient client = new ObsidianClient( consumerToken );
-
-		RequestToken requestToken = tokenRepository.getRequestToken( key );
-		client.setRequestToken( requestToken );
-
-		AccessToken accessToken = client.getAccessToken( verifier );
-		tokenRepository.setAccessToken( key, accessToken );
-
-		client.setAccessToken( accessToken );
-
-		return client;
-
+	@Override
+	protected ObsidianClient createInstance( ConsumerToken consumerToken ) {
+		return new ObsidianClient( consumerToken );
 	}
 
+	@Override
+	protected ObsidianClient createInstance( ConsumerToken consumerToken, String callbackUrl ) {
+		return new ObsidianClient( consumerToken, callbackUrl );
+	}
 }
